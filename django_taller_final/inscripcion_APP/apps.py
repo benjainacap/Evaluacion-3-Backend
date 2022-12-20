1bc6bc75738a0ca158cbcd8adafaac9b8692b24e
benjainacap <84517066+benjainacap@users.noreply.github.com>
2022-12-20 19:36:48 -0300

    Delete django_taller_final directory

--- django_taller_final/inscripcion_APP/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InscripcionAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inscripcion_APP'
--- django_taller_final/inscripcion_APP/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from inscripcion_APP.models import Inscripcion
-
-class FormInscripcion(forms.ModelForm):
-    class Meta:
-        model = Inscripcion
-        fields = '__all__'
